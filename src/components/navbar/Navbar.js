c1ada5d6504b92222583b500dec0cd1392935189
sfrancop <s.francop@uniandes.edu.co>
2022-08-25 14:52:29 -0500

    footer section

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import "./Navbar.css"
+import Logo from "../../images/logo.png"
 
 const Navbar = () => {
   return (
     <div className='n-container'>
       <div className='n-wrapper'>
         <div className='n-left'>
-        <div className='n-logo'></div>
+          <img src={Logo}></img>
         </div>
         <div className='n-right'>
           <div className='n-list'>
-            <ul>
+            <ul className='n-list-elements'>
               <li>¿Quiénes somos?</li>
-              <li> Únete</li>
               <li> Beneficios </li>
               <li> Modelo BASS</li>
-              <li>Contáctanos</li>
             </ul>
           </div>
         </div>
-        
-  
       </div>
     </div>
   )
--- src/components/quienes_somos/QuienesSomos.js
+++ src/components/quienes_somos/QuienesSomos.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import "./QuienesSomos.css"
-import celular from "../../images/celular.png"
+import Celular from "../../images/celular.png"
 
 const QuienesSomos = () => {
   return (
     <div className='q-container' id='QuienesSomos'>
       <div className='q-left'>
-        <p className='q-title'>¿Quiénes somos?</p>
-        <img className='q-left-image' src={celular}/>
+        <div className='q-left-up'>
+          <span className='q-title'>¿Quiénes somos?</span>
+        </div>
+        <div className='q-left-down'>
+        </div>
+
       </div>
       <div className='q-right'>
         <div className='q-right-text'>
           <p className='q-right-text-title'>Una solución práctica, segura, asequible y accesible</p>
           <p className='q-right-text-paragraph'>
-            <div className='first-p'>Somos una plataforma de movilidad digital que implementa un nuevo método de <span className='text-strong'> carpooling privado </span> y exclusivo para cada comunidad.</div>
-            <br></br><br></br>
+            <div className='first-p'>Somos una plataforma de movilidad digital que implementa un nuevo método de <span className='text-strong'>carpooling privado</span>  y exclusivo para cada comunidad.</div>
+            <br></br>
             <div>Estamos enfocados en  <span className='text-strong'>aumentar el  bienestar</span>  de las comunidades universitarias y todos sus integrantes al mejorar el actual sistema de movilidad en cuanto a calidad y disponibilidad.</div></p>
         </div>
       </div>
